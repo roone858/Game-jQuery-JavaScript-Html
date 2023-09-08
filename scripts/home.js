21f93dd699f6867c6e96f7e89b1653c1f7935ff8
roone858 <MahmoudG.dev@gmail.com>
2023-09-09 00:15:20 +0300

    add jquery instaed of dom in Home

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,9 @@
+const kidsImages = [
+  "../assets/image/home/kids/1-2.webp",
+  "../assets/image/home/kids/2-2.webp",
+  "../assets/image/home/kids/3-3.webp",
+  "../assets/image/home/kids/4-2.webp",
+];
 const imagesSrc = [
   {
     src: "./assets/image/home/icons/1a.webp",
@@ -17,88 +23,80 @@ const imagesSrc = [
   },
 ];
 
-const slider = document.querySelector(".slider");
+$(document).ready(function () {
+  const slider = $(".slider");
 
-let indexOne = 0,
-  indexTow = 1,
-  indexThree = 2,
-  indexFour = 3;
-changeSlides();
-setInterval(() => {
-  indexOne++;
-  indexTow++;
-  indexThree++;
-  indexFour++;
+  let indexOne = 0,
+    indexTow = 1,
+    indexThree = 2,
+    indexFour = 3;
   changeSlides();
-}, 8000);
+  setInterval(() => {
+    indexOne++;
+    indexTow++;
+    indexThree++;
+    indexFour++;
+    changeSlides();
+  }, 8000);
 
-document.getElementById("next-btn").addEventListener("click", () => {
-  indexOne++;
-  indexTow++;
-  indexThree++;
-  indexFour++;
-  changeSlides();
-});
-document.getElementById("prev-btn").addEventListener("click", () => {
-  let pointer = indexOne;
-  indexOne = indexFour;
-  indexFour = indexThree;
-  indexThree = indexTow;
-  indexTow = pointer;
-  changeSlides();
-});
-
-function changeSlides() {
-  if (indexOne > 3) indexOne = 0;
-  if (indexTow > 3) indexTow = 0;
-  if (indexThree > 3) indexThree = 0;
-  if (indexFour > 3) indexFour = 0;
-  if (indexOne < 0) indexOne = 3;
-  if (indexTow < 0) indexTow = 2;
-  if (indexThree < 0) indexThree = 1;
-  if (indexFour < 0) indexFour = 0;
-  slider.innerHTML = "";
-  slider.appendChild(
-    CreateSliderIcon(imagesSrc[indexOne].src, imagesSrc[indexOne].text)
-  );
-  slider.appendChild(
-    CreateSliderIcon(imagesSrc[indexTow].src, imagesSrc[indexTow].text)
-  );
-  slider.appendChild(
-    CreateSliderIcon(imagesSrc[indexThree].src, imagesSrc[indexThree].text)
-  );
-  slider.appendChild(
-     CreateSliderIcon(imagesSrc[indexFour].src, imagesSrc[indexFour].text)
-   );
-}
-function CreateSliderIcon(src, text) {
-  const card = document.createElement("div");
-  card.setAttribute("class", "icon-card");
-  const cardImg = document.createElement("img");
-  cardImg.setAttribute("src", "./assets/ss/s");
-  const cardSpan = document.createElement("span");
-  cardSpan.innerText = text;
-  cardImg.setAttribute("src", src);
-  card.appendChild(cardImg);
-  card.appendChild(cardSpan);
-  return card;
-}
-
-const kidsImages = [
-  "../assets/image/home/kids/1-2.webp",
-  "../assets/image/home/kids/2-2.webp",
-  "../assets/image/home/kids/3-3.webp",
-  "../assets/image/home/kids/4-2.webp",
-];
-const panelBtns = document.querySelectorAll(
-  ".panels .dynamic-ads .btns button "
-);
-panelBtns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    panelBtns.forEach((btn) => btn.classList.remove("open-ads-btn"));
-    btn.classList.add("open-ads-btn");
-    document.querySelector(
-      ".panels .dynamic-ads"
-    ).style.backgroundImage = `url(${kidsImages[index]})`;
+  $("#next-btn").on("click", () => {
+    indexOne++;
+    indexTow++;
+    indexThree++;
+    indexFour++;
+    changeSlides();
   });
+  $("#prev-btn").on("click", () => {
+    let pointer = indexOne;
+    indexOne = indexFour;
+    indexFour = indexThree;
+    indexThree = indexTow;
+    indexTow = pointer;
+    changeSlides();
+  });
+
+  function changeSlides() {
+    if (indexOne > 3) indexOne = 0;
+    if (indexTow > 3) indexTow = 0;
+    if (indexThree > 3) indexThree = 0;
+    if (indexFour > 3) indexFour = 0;
+    if (indexOne < 0) indexOne = 3;
+    if (indexTow < 0) indexTow = 2;
+    if (indexThree < 0) indexThree = 1;
+    if (indexFour < 0) indexFour = 0;
+    slider.html("");
+    slider.append(
+      CreateSliderIcon(imagesSrc[indexOne].src, imagesSrc[indexOne].text)
+    );
+    slider.append(
+      CreateSliderIcon(imagesSrc[indexTow].src, imagesSrc[indexTow].text)
+    );
+    slider.append(
+      CreateSliderIcon(imagesSrc[indexThree].src, imagesSrc[indexThree].text)
+    );
+    slider.append(
+      CreateSliderIcon(imagesSrc[indexFour].src, imagesSrc[indexFour].text)
+    );
+  }
+  function CreateSliderIcon(src, text) {
+    const card = $("<div>").addClass("icon-card");
+    const cardImg = $("<img>").attr("src", src);
+    const cardSpan = $("<span>").text(text);
+    card.append(cardImg, cardSpan);
+    return card;
+  }
+
+    const panelBtns = $(".panels .dynamic-ads .btns button");
+  
+    panelBtns.on("click", function() {
+      panelBtns.removeClass("open-ads-btn");
+  
+      $(this).addClass("open-ads-btn");
+  
+      const index = panelBtns.index(this);
+  
+      $(".panels .dynamic-ads").css("background-image", `url(${kidsImages[index]})`);
+    });
+ 
+  
 });
